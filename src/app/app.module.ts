0b2c3ebe1a69ec3fce23a9b195cc3d7376c1efec
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-19 12:24:46 +0300

    Added Quote form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuoteFormComponent } from './components/quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
