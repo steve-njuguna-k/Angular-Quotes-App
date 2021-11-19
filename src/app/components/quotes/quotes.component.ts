867720b706e36f32df25ee571f2026bdf087bbc4
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-19 13:27:32 +0300

    Renamed the Quote class to Quotes class

--- src/app/components/quotes/quotes.component.ts
+++ src/app/components/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from 'src/app/models/quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quotes.component.css']
 })
 export class QuotesComponent implements OnInit {
+
+  quote: Quotes[] = [
+    new Quotes(1, 'Living Life', 'You only live once, but if you do it right, once is enough', 'Mae West', 'Steve Njuguna'),
+  ];
 
   constructor() { }
 
--- src/app/models/quotes.ts
+++ src/app/models/quotes.ts
@@ -1,4 +1,4 @@
-export class Quote {
+export class Quotes {
     showDetails: boolean;
     constructor(public id: number,public quoteTitle: string,public quoteDescription: string, public quoteAuthor: string, public postAuthor: string){
       this.showDetails=false;
