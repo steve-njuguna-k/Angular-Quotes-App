4d1eaca7190ddda4b020f8e9166c07c12673aeb1
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-19 13:18:18 +0300

    Added quote properties in the Quote class

--- src/app/models/quote.ts
+++ src/app/models/quote.ts
@@ -1,2 +1,6 @@
 export class Quote {
+    showDetails: boolean;
+    constructor(public id: number,public quoteTitle: string,public quoteDescription: string, public quoteAuthor: string, public postAuthor: string){
+      this.showDetails=false;
+    }
 }
