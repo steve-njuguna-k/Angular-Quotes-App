a33228c1de77b53483d4f93e7839d19c09d11f24
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-20 20:14:44 +0300

    Added directive to quote card

--- src/app/directives/quote-highlight.directive.ts
+++ src/app/directives/quote-highlight.directive.ts
@@ -1,10 +1,14 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appQuoteHighlight]'
 })
 export class QuoteHighlightDirective {
 
-  constructor() { }
+  constructor(elem:ElementRef) {
+    //elem.nativeElement.style.backgroundColor="#00acee",
+    //elem.nativeElement.style.color="#ffffff",
+    elem.nativeElement.style.border = "2px double #03d87f";
+  }
 
 }
