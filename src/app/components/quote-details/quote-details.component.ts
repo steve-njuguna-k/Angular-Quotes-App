a09b28e0db666ae846da2708199491fe679a1294
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-21 10:59:09 +0300

    Made changes to the Quotes Detail component

--- src/app/components/quote-details/quote-details.component.ts
+++ src/app/components/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Quotes } from 'src/app/models/quotes';
 
 @Component({
@@ -10,19 +10,6 @@ export class QuoteDetailsComponent implements OnInit {
 
   @Input()
   quote!: Quotes;
-  @Output() toDelete = new EventEmitter<boolean>();
-
-  likes(){
-    this.quote.likes += 1;
-  }
-
-  dislikes(){
-    this.quote.dislikes += 1;
-  }
-
-  quoteDelete(complete:boolean){
-    this.toDelete.emit(complete);
-  }
 
   constructor() { }
 
