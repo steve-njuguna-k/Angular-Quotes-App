27b8cc4b0d70cbb8e993765aa7e264cd00ac7af1
Steve Njuguna <stevenjuguna93@gmail.com>
2021-11-21 11:04:52 +0300

    Adjusted border pixels in highlight directive

--- src/app/directives/quote-highlight.directive.ts
+++ src/app/directives/quote-highlight.directive.ts
@@ -8,7 +8,7 @@ export class QuoteHighlightDirective {
   constructor(elem:ElementRef) {
     //elem.nativeElement.style.backgroundColor="#00acee",
     //elem.nativeElement.style.color="#ffffff",
-    elem.nativeElement.style.border = "2px double #03d87f";
+    elem.nativeElement.style.border = "1px double #03d87f";
   }
 
 }
